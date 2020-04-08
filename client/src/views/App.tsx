4499f848fd43141706050c9b1c898439fea2a427
Aleaxander Bonilla <adbonilla.ad@gmail.com>
2020-04-08 17:27:30 -0500

    Web client created

---
+++ client/src/views/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+
+function App() {
+  return (
+    <Grid
+      className="root"
+      container
+      direction="row"
+      justify="center"
+      alignItems="center"
+      style={{ height: '100vh' }}
+    >
+      <h1>shark</h1>
+    </Grid>
+  )
+}
+
+export default App
